2d408830c39526758e211fb1ad00fac6c5db0f16
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-12 23:23:17 +0200

    added react-icons

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
@@ -25,7 +26,14 @@ const Navbar = () => {
               <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
             </Link>
           </ul>
+          <div className='md:hidden'>
+            <AiOutlineMenu size={25} />
+          </div>
         </div>
+      </div>
+
+      <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+        <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3]'></div>
       </div>
     </div>
   )
