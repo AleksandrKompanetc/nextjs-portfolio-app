e7125c7c20c3c2a1972d62da67a73831c3a1ba61
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-12 22:55:21 +0200

    added logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <Image src='/assets/logo.svg' alt='Logo' width={50} height={50} />
+      </div>
+    </div>
   )
 }
 
