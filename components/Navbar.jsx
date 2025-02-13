6b5e5d8ece746c00184b00d44dc94650c518ac91
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-13 22:08:01 +0200

    added function handleNav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [nav, setNav] = useState(false)
 
   const handleNav = () => {
-    setNav(true)
+    setNav(!nav)
   }
 
   return (
@@ -34,18 +34,19 @@ const Navbar = () => {
               <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
             </Link>
           </ul>
-          <div className='md:hidden'>
+          <div onClick={handleNav} className='md:hidden'>
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
 
-      <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
-        <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+      <div className={!nav ? 'fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+        <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+                            : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
           <div>
             <div className='flex w-full items-center justify-between'>
               <Image src='/assets/logo.svg' width={50} height={50} alt='/' />
-              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+              <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                 <AiOutlineClose />
               </div>
             </div>
