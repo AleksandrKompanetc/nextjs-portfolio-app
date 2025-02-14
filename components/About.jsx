270d802f48736fd52ffa5f538a081fd395fa440c
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-14 23:40:10 +0200

    changed styles for About

--- components/About.jsx
+++ components/About.jsx
@@ -5,23 +5,23 @@ const About = () => {
     <div className='w-full md:h-screen p-2 flex items-center py-16'>
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
         <div className='col-span-2'>
-          <p>About</p>
-          <h2>Who I Am</h2>
-          <p>//I am not your normal developer</p>
-          <p>I have spent the last 12 years in the fire service working as a proffesional firefighter & paramedic. 
+          <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
+          <h2 className='py-4'>Who I Am</h2>
+          <p className='py-2 text-gray-600'>//I am not your normal developer</p>
+          <p className='py-2 text-gray-600'>I have spent the last 12 years in the fire service working as a proffesional firefighter & paramedic. 
             I have always had a knack for technology and working with computers. In 2019 I started working with 
             HTML & CSS to make some minor edits on a small business website that I was operating. What I thought 
             was just a few small edits turned into a love for programming.
           </p>
-          <p>Fascinated with how intricate programming can be I was quickly drown to learn more. I started learning 
+          <p className='py-2 text-gray-600'>Fascinated with how intricate programming can be I was quickly drown to learn more. I started learning 
             javascript and was even more enthused with macking website interactive. I then started freelancing for 
             e-commerse companies on the Shopify platform. I am now spanding my time building projects with React JS, 
             Firebase and learning new technologies.
           </p>
-          <p>Check out some of my latest projects.</p>
+          <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects.</p>
         </div>
         <div>
-          <img src="/assets/proj4.jpg" alt="" />
+          <img src="/assets/noutbook.jpg" alt="noutbook" />
         </div>
       </div>
     </div>
