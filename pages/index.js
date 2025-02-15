5815b85175fde5da503db1e8dbf12dcaba433f1b
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-15 22:50:38 +0200

    added component Projects

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import Main from "@/components/Main";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export default function Home() {
       <Main />
       <About />
       <Skills />
+      <Projects />
     </div>
   );
 }
