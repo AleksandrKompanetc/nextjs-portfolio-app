631cc4ab6bcfaccb48f4c0349ce2dd87cfb51632
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-15 23:40:58 +0200

    added changes

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import React from 'react'
-import propertyImg from '.public/assets/project1.jpg'
 
 const Projects = () => {
   return (
@@ -10,7 +9,7 @@ const Projects = () => {
         <h2 className='py-4'>What i've Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
           <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-            <Image src={propertyImg} />
+            <Image src='/assets/project1.jpg' width={300} height={100} />
           </div>
         </div>
       </div>
