417310f3f2f1f0101299fbe9002cf07b5e9612e8
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-16 21:58:52 +0200

    added component ProjectItem

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
+import Link from 'next/link'
+import ProjectItem from './ProjectItem'
 
 const Projects = () => {
   return (
@@ -8,9 +10,7 @@ const Projects = () => {
         <p className='text-xl tracking-widest uppercase text-[#5651e5] '>Projects</p>
         <h2 className='py-4'>What i've Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
-          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-            <Image src='/assets/project1.jpg' width={300} height={100} />
-          </div>
+          <ProjectItem />
         </div>
       </div>
     </div>
