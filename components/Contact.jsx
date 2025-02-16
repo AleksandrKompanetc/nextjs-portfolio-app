4ddbcf6547b3e5bf94b6a6d4b00b7d99adb36c23
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-16 23:37:54 +0200

    added right side of the Contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -36,6 +36,13 @@ const Contact = () => {
               </div>
             </div>
           </div>
+          <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
+            <div className='p-4'>
+              <form action="">
+                <div className='grid '></div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
