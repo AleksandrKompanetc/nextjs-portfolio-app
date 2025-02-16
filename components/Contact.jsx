5007962b4ac635e83fd67f93037dd0ae6ee08d89
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-16 23:24:11 +0200

    added changes for Contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,7 +3,41 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='w-full lg:h-screen'>
-
+      <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Contact</p>
+        <h2 className='py-4'>Get In Touch</h2>
+        <div className='grid lg:grid-cols-5 gap-8'>
+          <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
+            <div className='lg:p-4 h-full'>
+              <div className=''>
+                <img className='rounded-xl hover:scale-105 ease-in duration-300' src="..public/assets/comp.jpg" alt="" />
+              </div>
+              <div>
+                <h2 className='py-2 '>Name here</h2>
+                <p>Front-End Developer</p>
+                <p className='py-4'>I am available for freelance or full-time positions. Contact me and let's talk.</p>
+              </div>
+            </div>
+            <div>
+              <p className='uppercase pt-8'>Connect With Me</p>
+              <div className='flex items-center justify-between py-4'>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                  <FaLinkedinIn />
+                </div>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                  <FaGithub />
+                </div>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                  <AiOutlineMail />
+                </div>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                  <BsFillPersonLinesFill />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
