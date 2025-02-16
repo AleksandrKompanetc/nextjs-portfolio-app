6e973c0fc0f2244bd4df8878960658f1243dad80
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-16 23:41:20 +0200

    creating form for the Contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -39,7 +39,11 @@ const Contact = () => {
           <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
             <div className='p-4'>
               <form action="">
-                <div className='grid '></div>
+                <div className='grid md:grid-cols-2 gap-4 w-full py-2 '>
+                  <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2 ' htmlFor="">Name</label>
+                  </div>
+                </div>
               </form>
             </div>
           </div>
