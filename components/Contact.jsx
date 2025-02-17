144bd2134c90b63b23246c92f987588f1023f143
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-17 22:57:42 +0200

    added buttons

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,6 +3,8 @@ import { FaLinkedinIn } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
 import { AiOutlineMail } from 'react-icons/ai'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { HiOutlineChevronDoubleUp } from 'react-icons/hi'
+import Link from 'next/link'
 
 const Contact = () => {
   return (
@@ -66,10 +68,17 @@ const Contact = () => {
                   <label className='uppercase text-sm py-2'>Message</label>
                   <textarea className='border-2 rounded-lg p-3 border-gray-300' rows='10'></textarea>
                 </div>
+                <button className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
               </form>
             </div>
           </div>
-
+        </div>
+        <div className='flex justify-center py-12 '>
+          <Link href='/'>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <HiOutlineChevronDoubleUp className='text-[#5651e5]' size={30} />
+            </div>
+          </Link>
         </div>
       </div>
     </div>
