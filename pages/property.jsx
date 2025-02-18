456acc66d081f623d09be5fec06e78b32ad0c8eb
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-18 21:36:11 +0200

    added changes

--- pages/property.jsx
+++ pages/property.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
+// import propertyImg from '../public/assets/projects/property.jpg'
 
 const property = () => {
   return (
     <div className='w-full'>
-      <div className='w-screen h-[30vh] lg:w-[40vh] relative '>
-        <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[30vh] bg-black/80 z-10'></div>
+      <div className='w-screen h-[30vh] lg:h-[40vh] relative '>
+        <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[30vh] bg-black/80 z-10' />
+        
       </div>
     </div>
   )
